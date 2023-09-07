fca551a0ae4bd89ec88df810f0ce191a43695726
Robertto-Oliveira <robertto.bh.mg@gmail.com>
2023-09-07 14:56:08 -0300

    last_update

--- javascript.js
+++ javascript.js
@@ -6,8 +6,6 @@ const formPesquisar = document.getElementById('form-pesquisar');
 // Captura o botão de cancelar pelo ID
 const btnCancelar = document.querySelector('.btn-cancelar');
 
-
-
 // Adiciona um evento de clique ao botão de cancelar
 btnCancelar.addEventListener('click', function (event) {
     event.preventDefault();
@@ -22,6 +20,23 @@ btnCancelar.addEventListener('click', function (event) {
 btnCancelar.addEventListener('click', function (event) {
     event.preventDefault();
     formPesquisar.reset();
+});
+
+//CADASTRAR
+
+
+//PESQUISAR
+
+const btnPesquisar = document.querySelector('.btn-pesquisar');
+        //const btnCancelar = document.querySelector('.btn-limpar');
+        
+btnPesquisar.addEventListener('click', function () {
+    const nomeSerie = document.getElementById('serie-input').value;
+    const temporada = document.getElementById('season-input').value;
+    const episodio = document.getElementById('episode-input').value;
+            
+// Chamar a função que realiza a pesquisa e exibe os resultados
+    realizarPesquisa(nomeSerie, temporada, episodio);
 });
 
 
